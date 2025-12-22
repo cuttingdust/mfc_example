f28c6876b0decad6a16e9b32a5b60ff9aeaabb7c
handabao <18862963156@163.com>
2025-12-22 14:17:52 +0800

    CHtmlView 原先支持的是IE 但是后来被启用了 CHtmlView也被弃用了 替代方案考虑： WebBrowser WebView2 CEF3

---
+++ src/MyExplorer/MyExplorerView.cpp
@@ -0,0 +1,82 @@
+﻿
+// MyExplorerView.cpp: CMyExplorerView 类的实现
+//
+
+#include "pch.h"
+#include "framework.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "MyExplorer.h"
+#endif
+
+#include "MyExplorerDoc.h"
+#include "MyExplorerView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CMyExplorerView
+
+IMPLEMENT_DYNCREATE(CMyExplorerView, CHtmlView)
+
+BEGIN_MESSAGE_MAP(CMyExplorerView, CHtmlView)
+// 标准打印命令
+ON_COMMAND(ID_FILE_PRINT, &CHtmlView::OnFilePrint)
+END_MESSAGE_MAP()
+
+// CMyExplorerView 构造/析构
+
+CMyExplorerView::CMyExplorerView() noexcept
+{
+    // TODO: 在此处添加构造代码
+}
+
+CMyExplorerView::~CMyExplorerView()
+{
+}
+
+BOOL CMyExplorerView::PreCreateWindow(CREATESTRUCT& cs)
+{
+    // TODO: 在此处通过修改
+    //  CREATESTRUCT cs 来修改窗口类或样式
+
+    return CHtmlView::PreCreateWindow(cs);
+}
+
+void CMyExplorerView::OnInitialUpdate()
+{
+    CHtmlView::OnInitialUpdate();
+
+    SetSilent(TRUE);
+    Navigate2(L"http://www.microsoft.com/", NULL, NULL); // 微软官网
+}
+
+
+// CMyExplorerView 打印
+
+
+// CMyExplorerView 诊断
+
+#ifdef _DEBUG
+void CMyExplorerView::AssertValid() const
+{
+    CHtmlView::AssertValid();
+}
+
+void CMyExplorerView::Dump(CDumpContext& dc) const
+{
+    CHtmlView::Dump(dc);
+}
+
+CMyExplorerDoc* CMyExplorerView::GetDocument() const // 非调试版本是内联的
+{
+    ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CMyExplorerDoc)));
+    return (CMyExplorerDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CMyExplorerView 消息处理程序
---
+++ src/MyExplorer/MyExplorerView.h
@@ -0,0 +1,46 @@
+﻿
+// MyExplorerView.h: CMyExplorerView 类的接口
+//
+
+#pragma once
+
+
+class CMyExplorerView : public CHtmlView
+{
+protected: // 仅从序列化创建
+	CMyExplorerView() noexcept;
+	DECLARE_DYNCREATE(CMyExplorerView)
+
+// 特性
+public:
+	CMyExplorerDoc* GetDocument() const;
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+	virtual void OnInitialUpdate(); // 构造后第一次调用
+
+// 实现
+public:
+	virtual ~CMyExplorerView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // MyExplorerView.cpp 中的调试版本
+inline CMyExplorerDoc* CMyExplorerView::GetDocument() const
+   { return reinterpret_cast<CMyExplorerDoc*>(m_pDocument); }
+#endif
+
